1b1823125e2879bb901c5e796d0e5eaaddf6efea
Joachim5537 <45621732+Joachim5537@users.noreply.github.com>
2019-11-13 01:51:22 +0800

    Update reminder.js
    Added closest reminder list function

--- reminders_webext/reminder.js
+++ reminders_webext/reminder.js
@@ -92,6 +92,58 @@ const setReminderList = response => {
   }
 }
 
+const getMostCloseReminder = () =>{
+
+  while (domElements.reminderHolder.firstChild) {
+    domElements.reminderHolder.removeChild(domElements.reminderHolder.firstChild);
+}
+
+    /// call your function here
+  
+  window.setInterval(function(){
+  
+      const gettingItem = browser.storage.local.get('tslReminders'); 
+    
+      gettingItem.then((res) => {
+        var now = Date.parse(new Date);
+        var close_time = ''
+
+        for(let item in res['tslReminders']){
+          var list_time = parseInt(item.split('_')[1]);
+          var name = '';
+          
+          if(close_time == ''){
+            close_time = new Date(list_time);
+            var hour = close_time.getHours();
+            name = res['tslReminders'][item];
+          }else{
+
+            if( (list_time - now) < close_time) {
+              close_time = list_time;
+              name = res['tslReminders'][item];
+            }
+
+          }
+              var date = new Date(close_time);
+              var year = date.getFullYear();
+              var month = ("0" + (date.getMonth() + 1)).slice(-2);
+              var day = ("0" + date.getDate()).slice(-2);
+              var hour = date.getHours();
+              var minutes = date.getMinutes();
+              var seconds = date.getSeconds();
+
+              var display_time = year+'-'+month+'-'+day+' '+hour+':'+minutes;
+              // alert(hour)
+          document.getElementById("mostCloseReminderTime").innerHTML = "Closest Reminder :"+name+"<br> Time :"+(display_time);
+        
+        }
+
+      })
+    }, 1000);
+}
+
+window.onload = getMostCloseReminder();
+
 const removeReminder = (item, reminder, tslReminders) => {
   domElements.reminderHolder.removeChild(reminder)
   delete tslReminders[item]
